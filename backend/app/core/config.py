f604a45548faa4a2bdf5b31f6da2aaa6892a558a
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-03-11 11:09:44 +0200

    Add application configuration

---
+++ backend/app/core/config.py
@@ -0,0 +1,33 @@
+import secrets
+
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    API_PREFIX: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    BACKEND_CORS_ORIGINS: list[str] = ["http://localhost:8000"]
+
+    @classmethod
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    def assemble_cors_origins(cls, origins: str | list[str]) -> list[str] | str:
+        if isinstance(origins, str) and not origins.startswith("["):
+            return [origin.strip() for origin in origins.split(",")]
+        return origins
+
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "postgres"
+    POSTGRES_PORT: str = "5432"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+    )
+
+
+settings = Settings()
