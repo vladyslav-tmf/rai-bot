146ef988ee19bd5191e58aa623aaa9c495652bb0
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-03-11 11:29:35 +0200

    Configure async database connection with SQLAlchemy and AsyncPG

---
+++ backend/app/db/session.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from backend.app.core.config import settings
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}"
+    f"@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+)
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False, future=True)
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+
+        finally:
+            await session.close()
