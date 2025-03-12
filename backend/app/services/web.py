6348529826424b084e87f6d7a0c0250b42199394
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-03-12 13:29:46 +0200

    Exclude versions directory from flake8

--- backend/app/services/web.py
+++ backend/app/services/web.py
@@ -36,7 +36,7 @@ class WebService:
             is_valid = all([result.scheme in ["http", "https"], result.netloc])
             return is_valid
 
-        except ValueError as error:
+        except ValueError:
             return False
 
     async def get_webpage_content(self, url: str) -> str | None:
@@ -59,5 +59,5 @@ class WebService:
 
             return text
 
-        except httpx.HTTPError as error:
+        except httpx.HTTPError:
             return None
